208910f84ddbd3196b4c4ba124ff034689090075
Sebastien Ramsay <ramsa0130@algonquinlive.com>
2022-11-10 22:38:38 -0500

    Completed up to LayoutManager in step 2.

--- app/src/main/java/com/example/sebastiensandroidlabs/ChatRoom.java
+++ app/src/main/java/com/example/sebastiensandroidlabs/ChatRoom.java
@@ -2,17 +2,24 @@ package com.example.sebastiensandroidlabs;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.sebastiensandroidlabs.databinding.ActivityChatRoomBinding;
+import com.example.sebastiensandroidlabs.databinding.SentMessageBinding;
+
+import java.util.ArrayList;
 
 public class ChatRoom extends AppCompatActivity {
 
     ActivityChatRoomBinding binding;
+    ArrayList<String> messages = new ArrayList<>();
+    private RecyclerView.Adapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,31 +29,58 @@ public class ChatRoom extends AppCompatActivity {
         binding = ActivityChatRoomBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        binding.recyclerView.setAdapter(new RecyclerView.Adapter<MyRowHolder>() {
+
+        binding.recyclerView.setAdapter(myAdapter = new RecyclerView.Adapter<MyRowHolder>() {
             @NonNull
             @Override
             public MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                return null;
+                SentMessageBinding binding = SentMessageBinding.inflate(getLayoutInflater());
+                return new MyRowHolder(binding.getRoot());
             }
 
             @Override
             public void onBindViewHolder(@NonNull MyRowHolder holder, int position) {
-
+                String obj = messages.get(position);
+                holder.messageText.setText(obj);
+                holder.timeText.setText("");
             }
 
             @Override
             public int getItemCount() {
+                return messages.size();
+            }
+
+            public int getItemViewType(int position){
                 return 0;
             }
         });
 
 
+        binding.sendButton.setOnClickListener(click -> {
+            messages.add(binding.textInput.getText().toString());
+
+            myAdapter.notifyItemInserted(messages.size() - 1);
+                    //notifyItemRemoved
+                    //notifyDatasetChanged
+
+            binding.textInput.setText("");
+        });
+
     }
 }
 
 class MyRowHolder extends RecyclerView.ViewHolder {
+
+    TextView messageText;
+    TextView timeText;
+
+
     public MyRowHolder(@NonNull View itemView) {
+
         super(itemView);
+        messageText = itemView.findViewById(R.id.messageText);
+        timeText = itemView.findViewById(R.id.timeText);
     }
 }
